3a42dc93915295102a9fd02a9d116a9b7c3763a1
asghari <asghari1368@gmail.com>
2023-07-28 18:50:54 +0330

    feature(store activity): add a route for storing activity

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,6 @@ Route::get('activities', [ActivityController::class, 'getAllActivities']);
 Route::get('activities/{type}', [ActivityController::class, 'getActivitiesByType']);
 Route::get('activities/{type}/total-distance', [ActivityController::class, 'getTotalDistanceByType']);
 Route::get('activities/{type}/total-time', [ActivityController::class, 'getTotalTimeByType']);
+
+Route::post('/activities', [ActivityController::class, 'store']);
+
