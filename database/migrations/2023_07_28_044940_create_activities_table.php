cb3b2bfa7ae88c8ae85b56af7aa26ded2cf06531
asghari <asghari1368@gmail.com>
2023-07-28 09:20:18 +0330

    db(table): add activity table to migration

---
+++ database/migrations/2023_07_28_044940_create_activities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->enum('activity_type', ['running', 'cycling', 'walking', 'swimming', 'yoga']);
+            $table->date('activity_date');
+            $table->string('name');
+            $table->float('distance');
+            $table->enum('distance_unit', ['kilometers', 'miles', 'meters']);
+            $table->integer('elapsed_time'); // in seconds
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
