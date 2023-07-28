5eb26581c594dd4c69273c72d1d256cda221c3c2
asghari <asghari1368@gmail.com>
2023-07-28 13:05:10 +0330

    feature(activity controller): add basic method to ActivityController

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -1,26 +1,53 @@
 <?php
 
-use App\Http\Controllers\Controller;
+namespace App\Http\Controllers;
+
+use App\Models\Activity;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ActivityController extends Controller
 {
+
+    public function __construct(private Activity $activityModel)
+    {
+    }
+
     public function getAllActivities()
     {
-        /** TODO: complete the function body */
+        $activities = $this->activityModel->getAllActivities();
+        return response()->json($activities);
     }
 
     public function getActivitiesByType($type)
     {
-        /** TODO: complete the function body */
+        $activities = $this->activityModel->getActivitiesByType($type);
+        return response()->json($activities);
     }
 
     public function getTotalDistanceByType($type)
     {
-        /** TODO: complete the function body */
+        $totalDistance = $this->activityModel->getTotalDistanceByType($type);
+        return response()->json(['total_distance' => $totalDistance]);
     }
 
     public function getTotalTimeByType($type)
     {
-        /** TODO: complete the function body */
+        $totalTime = $this->activityModel->getTotalTimeByType($type);
+        return response()->json(['total_time' => $totalTime]);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'activity_type' => 'required|in:running,cycling,walking,swimming,yoga',
+            'activity_date' => 'required|date',
+            'name' => 'required|string',
+            'distance' => 'required|numeric|min:0',
+            'distance_unit' => 'required|in:kilometers,miles,meters',
+            'elapsed_time' => 'required|integer|min:0',
+        ]);
+        $activity = $this->activityModel->storeActivity($validatedData);
+        return response()->json($activity, Response::HTTP_CREATED);
     }
 }
