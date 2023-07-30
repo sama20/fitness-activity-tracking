c124a64eaf2884b77dead4269f5dfc5c60625612
sama20 <asghari1368@gmail.com>
2023-07-30 15:01:51 +0330

    refactor(ActivityServiceTest): adding mock data

--- tests/Unit/ActivityServiceTest.php
+++ tests/Unit/ActivityServiceTest.php
@@ -1,96 +1,70 @@
 <?php
+
 namespace Tests\Unit;
 
-use App\Models\Activity;
 use App\Models\Enums\ActivityType;
-use App\Models\Enums\DistanceUnit;
 use App\Services\ActivityService;
+use App\Models\Activity;
+use App\Models\Enums\DistanceUnit;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Tests\TestCase;
+use PHPUnit\Framework\TestCase;
+use Mockery;
 
 class ActivityServiceTest extends TestCase
 {
-    use DatabaseTransactions;
+    private Activity $activityModel;
     private ActivityService $activityService;
-    private int $totalTimeBefore = 0;
-    private Collection $allActivitiesBefore ;
-    private Collection $runningActivitiesBefore ;
-
-    private int $totalDistanceBefore;
-
-    private Activity $sampleActivity;
-
-    public function __construct(string $name)
-    {
-        parent::__construct($name);
-        $this->activityService = new ActivityService(new Activity());
-    }
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->allActivitiesBefore = $this->activityService->getAllActivities();
-        $this->runningActivitiesBefore = $this->activityService->getActivitiesByType(ActivityType::Running->value);
-        $this->totalDistanceBefore = $this->activityService->getTotalDistanceByType(ActivityType::Running->value);
-        $this->totalTimeBefore = strtotime($this->activityService->getTotalTimeByType(ActivityType::Running->value));
 
+        $this->activityModel = Mockery::mock(Activity::class);
+        $this->activityService = new ActivityService($this->activityModel);
+    }
 
-        // Seed the database with some known data for testing
-        $this->sampleActivity = Activity::factory()->create([
-            'activity_type' => ActivityType::Running,
-            'elapsed_time' => 1000,
-            'distance_unit' => DistanceUnit::Kilometer,
-            'distance' => 2,
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        Mockery::close();
+    }
+
+    /** TODO:
+     * I ignore the below methods for testing. because they just call the Model that I tested them in ActivityTest.
+     * If in the future added some logic to the service you should make their tests.
+     * getAllActivities    getActivitiesByType     storeActivity
+     */
+
+    public function test_getTotalDistanceByType_returns_total_distance()
+    {
+        $type = ActivityType::Running->value;
+        $activities = new Collection([
+            (object) ['distance' => 1, 'distance_unit' => DistanceUnit::Kilometer->value],
+            (object) ['distance' => 500, 'distance_unit' => DistanceUnit::Meter->value],
         ]);
 
-        Activity::factory()->create([
-            'activity_type' => ActivityType::Running,
-            'elapsed_time' => 2600,
-            'distance_unit' => DistanceUnit::Meter,
-            'distance' => 700,
-        ]);
+        $this->activityModel->shouldReceive('getActivitiesByType')
+            ->with($type)
+            ->andReturn($activities);
 
-        Activity::factory()->create([
-            'activity_type' => ActivityType::Cycling,
-            'elapsed_time' => 1100,
-        ]);
+        $totalDistance = $this->activityService->getTotalDistanceByType($type);
+
+        $this->assertEquals(1500, $totalDistance);
     }
 
-    public function test_it_can_get_all_activities()
+    public function test_getTotalTimeByType_returns_total_time()
     {
+        $type = ActivityType::Running->value;
+        $totalTimeInSeconds = 3600;
 
-        $allActivities = $this->activityService->getAllActivities();
+        $this->activityModel->shouldReceive('sumElapsedTimeByType')
+            ->with($type)
+            ->andReturn($totalTimeInSeconds);
 
-        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $allActivities);
-        $this->assertEquals(3, count($allActivities) - count($this->allActivitiesBefore));
-    }
+        $totalTime = $this->activityService->getTotalTimeByType($type);
 
-    public function test_it_can_get_activities_by_type()
-    {
-        $runningActivities = $this->activityService->getActivitiesByType(ActivityType::Running->value);
-
-        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $runningActivities);
-        $this->assertEquals(2, count($runningActivities) - count($this->runningActivitiesBefore));
-    }
-
-    public function test_it_can_get_total_distance_by_type()
-    {
-        $totalDistance = $this->activityService->getTotalDistanceByType(ActivityType::Running->value);
-
-        $this->assertEquals(2700, floor($totalDistance - $this->totalDistanceBefore) ); // Assuming distance_unit is in 'km'
-    }
-
-    public function test_it_can_get_total_time_by_type()
-    {
-        $totalTime = $this->activityService->getTotalTimeByType(ActivityType::Running->value);
-
-        $this->assertEquals(3600, strtotime($totalTime)- $this->totalTimeBefore);
-    }
-
-    public function test_it_can_store_activity()
-    {
-        $this->assertInstanceOf(Activity::class, $this->sampleActivity);
-        $this->assertEquals(1000, $this->sampleActivity->elapsed_time);
+        $expectedTotalTime = '01:00:00';
+        $this->assertEquals($expectedTotalTime, $totalTime);
     }
 }
